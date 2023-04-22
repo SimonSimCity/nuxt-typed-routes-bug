f1cf609a32f06c62a4773378fb6009efa028c8d8
Simon Schick <simonsimcity@gmail.com>
2023-04-22 11:42:59 +0200

    Initial commmit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+      ["nuxt-typed-router", { strict: true }],
+      "@nuxtjs/i18n",
+    ],
+  });
+  
---
+++ pages/browse.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Browse</div>
+  <nuxt-link :to="{ name: 'playlist-private-id', params: { id: 0 } }"></nuxt-link>
+</template>
