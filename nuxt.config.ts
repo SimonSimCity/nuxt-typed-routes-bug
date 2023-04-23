1fe1570874fb6715890f9b1c0b77dd5812d09279
Simon Schick <simonsimcity@gmail.com>
2023-04-23 10:36:36 +0200

    Set i18n config beta-10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-      ["nuxt-typed-router", { strict: true }],
-      "@nuxtjs/i18n",
-    ],
-  });
-  
+  modules: [
+    ["nuxt-typed-router", { strict: true }],
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    defaultLocale: "en",
+    vueI18n: {
+      legacy: false,
+      locale: "en",
+      fallbackLocale: "fr",
+      messages: {
+        en: {},
+        fr: {},
+      },
+    },
+  },
+});
